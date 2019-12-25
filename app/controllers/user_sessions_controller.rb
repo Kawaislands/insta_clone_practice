7f913ccd413b91e4ec20f6b61a911a7d7401d465
kawa <kawaislands@gmail.com>
2019-12-25 16:46:37 +0900

    微修正

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -3,8 +3,9 @@ class UserSessionsController < ApplicationController
 
   def create
     @user = login(params[:email], params[:password])
+
     if @user
-      redirect_back_or_to root_path, success: 'ログインしました'
+      redirect_back_or_to posts_path, success: 'ログインしました'
     else
       flash.now[:danger] = 'ログインに失敗しました'
       render :new
@@ -13,6 +14,6 @@ class UserSessionsController < ApplicationController
 
   def destroy
     logout
-    redirect_to root_path, success: 'ログアウトしました'
+    redirect_to login_path, success: 'ログアウトしました'
   end
 end
