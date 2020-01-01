6ac567d5f589ab21b948d3d30a185e6485a94d35
kawa <kawaislands@gmail.com>
2020-01-01 22:59:51 +0900

    いいね機能実装完了

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,12 +2,14 @@ class LikesController < ApplicationController
   before_action :require_login, only: %i[create destroy]
 
   def create
-    #@post = Post.find(params[:post_id])
-    current_user.like.create(post_id: params[:post_id])
-    #Like.create(user_id: current_user.id, post_id: params[:post_id])
+    @post = Post.find(params[:post_id])
+    #current_user.like.create(post_id: params[:post_id])
+    Like.create(user_id: current_user.id, post_id: params[:post_id])
   end
 
   def destroy
-    current_user.like.find(params[:post_id]).destroy
+    @post = Like.find(params[:id]).post
+    #current_user.like.find(params[:post_id]).destroy
+    Like.find(params[:id]).destroy
   end
 end
