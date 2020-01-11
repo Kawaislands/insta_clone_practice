b7b327e6c3989858046a9d91466c0b89ef26a0a3
kawa <kawaislands@gmail.com>
2020-01-11 22:08:11 +0900

    profile編集

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,5 +2,16 @@
 //= require popper
 //= require rails-ujs
 //= require bootstrap-material-design/dist/js/bootstrap-material-design.js
-//= require swiper/js/swiper.js
-//= require swiper.js
+function previewFileWithId(selector) {
+    const target = this.event.target;
+    const file = target.files[0];
+    const reader  = new FileReader();
+    reader.onloadend = function () {
+        selector.src = reader.result;
+    }
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        selector.src = "";
+    }
+}
